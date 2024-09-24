fe5ff520f1530630986c498f5c6a457a6208720a
David Williamson <david.williamson@varilink.co.uk>
2024-09-24 17:52:33 +0100

    VARIOUS CHANGES TO THE ADMIN DOCKER COMPOSE SERVICE
    As follows:
    
     - Remove the executable permission on the docker-entrypoint.sh script
       and pass it as a command to a bash shell instead.
    
     - Use a Python virtual environment, which might seem odd within a
       container but this way we simulate the live setup more accurately.
    
     - Install Ansible within the image so that we can use its templating
       engine. This allows us to align templates across this repository and
       the Ansible repository that does the live deployment.
    
     - Create the Django core database in its final destination within the
       container so we don't have to move and rename it after creation.
    
     - Move the migration steps and the creation of the Django superuser
       from the image creation to the entrypoint script so that we can use
       volume mappings from the host when we do so.
    
     - Reflect upgrades in both the base Debian release and also the Django
       framework and Python utilities.
    
     - Expand options to rsync to longer names for greater clarity.
    
     - Switch to using user www-data rather than root once we can.

--- admin/docker-entrypoint.sh
+++ admin/docker-entrypoint.sh
@@ -1,17 +1,41 @@
-#!/usr/bin/env bash
+# ------------------------------------------------------------------------------
+# admin/docker-entrypoint.sh
+# ------------------------------------------------------------------------------
+
+# Entrypoint script for this project's Docker Compose admin service.
+
+# ------
 
 set -e # exit immediately if a command exits with a non-zero status
 
-if [[ -f /data/db.sqlite3 ]]
+if [[ ! -f /database/core.db ]]
 then
 
-  mv /data/db.sqlite3 /database/core.db
-  chown www-data:www-data /database/core.db
+  # The Django core DB hasn't been created yet so create it now.
+
+  source ~/.venv/bin/activate
+  cd data
+  for app_label in 'admin' 'auth' 'contenttypes' 'sessions'
+  do
+
+    python3 manage.py migrate $app_label --database core_db
+
+  done
+
+  DJANGO_SUPERUSER_USERNAME=testuser \
+  DJANGO_SUPERUSER_PASSWORD=testpass \
+  DJANGO_SUPERUSER_EMAIL=testuser@localhost \
+  python3 manage.py createsuperuser --database core_db --noinput
 
 fi
 
-rsync -avu --delete                                                            \
-  /usr/local/lib/python3.7/dist-packages/django/contrib/admin/static/          \
+# Resynchronise the Django static files folder with the django-static volume,
+# which is mapped to /static in the container. That way if the build has been
+# altered to use an upgraded version of Django then the django-static volume
+# is updated to reflect any changes to the Djano static files.
+
+rsync --archive --delete --verbose                                             \
+  ~/.venv/lib/python3.11/site-packages/django/contrib/admin/static/            \
   /static/
 
 exec uwsgi "$@"
