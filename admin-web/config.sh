c8d428f937229b1197bc4c045ab762b701ddd26c
David Williamson <david.williamson@varilink.co.uk>
2026-01-09 15:53:53 +0000

    DEFINE A NEW ADMIN-WEB SERVICE
    We used to have the single web service serving from the main DATA web
    application and Django admin application interfaces but now we're using
    this entirely new web service for the latter.

---
+++ admin-web/config.sh
@@ -0,0 +1,18 @@
+# ------------------------------------------------------------------------------
+# admin_web/config.sh
+# ------------------------------------------------------------------------------
+
+# The entrypoint script for the Nginx image looks for shell scripts in the
+# /docker-entrypoint.d/ directory and runs them if it finds them. We copy this
+# shell script to that directory in order to customise the behaviour of the
+# app web service container, which is based on the Nginx image.
+
+# ------
+
+echo 'Running the project Nginx configuration script for the web admin service'
+
+if [ "${WORKER_PROCESSES}" ]; then
+  echo "Overriding default Nginx worker processes to ${WORKER_PROCESSES}"
+  sed -i "s/^worker_processes.*$/worker_processes  ${WORKER_PROCESSES};/"      \
+    /etc/nginx/nginx.conf
+fi
