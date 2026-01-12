2f76700cf2ba01027e358de6eb3b6276b7498126
David Williamson <david.williamson@varilink.co.uk>
2026-01-12 20:11:08 +0000

    ADD LOGGING SWITCH TO ADMIN WEB SERVICE STARTUP

--- admin-web/config.sh
+++ admin-web/config.sh
@@ -16,3 +16,10 @@ if [ "${WORKER_PROCESSES}" ]; then
   sed -i "s/^worker_processes.*$/worker_processes  ${WORKER_PROCESSES};/"      \
     /etc/nginx/nginx.conf
 fi
+
+if [ "${DATA_ADMIN_WEB_ERROR_LOG_LEVEL}" ]; then
+  echo "Setting Nginx log level to ${DATA_ADMIN_WEB_ERROR_LOG_LEVEL}"
+  log_file='\/var\/log\/nginx\/error.log'
+  sed -i "s/^error_log.*$/error_log ${log_file}                                \
+    ${DATA_ADMIN_WEB_ERROR_LOG_LEVEL};/" /etc/nginx/nginx.conf
+fi
